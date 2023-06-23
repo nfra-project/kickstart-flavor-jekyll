855ec9dcf22d29dd2d84cddbf0a6a192cbd1eefc
Matthias Leuffen <matthes@leuffen.de>
2023-06-23 13:32:58 +0200

    update

--- kickstart/flavor/build.d/50-install-apache-jekyll.sh
+++ kickstart/flavor/build.d/50-install-apache-jekyll.sh
@@ -13,7 +13,12 @@ apt-get -y install gettext apache2 psmisc \
 export GEM_HOME="/usr/bin/gems"
 gem install jekyll bundler
 
-
+# Install Node 14x
+curl -sL https://deb.nodesource.com/setup_20.x | sudo bash -
+sudo apt-get install -y nodejs
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 
 
 chmod -R 777 /var/www/html
