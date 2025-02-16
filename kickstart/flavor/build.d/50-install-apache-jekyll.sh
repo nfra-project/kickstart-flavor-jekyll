9644f7bf06fd7d18e773bcfe95751ab722f317e1
Matthias <m@tth.es>
2025-02-16 21:03:48 +0100

    updated to 22.04 - added kindergarden - added yarn

--- kickstart/flavor/build.d/50-install-apache-jekyll.sh
+++ kickstart/flavor/build.d/50-install-apache-jekyll.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get -y install gettext apache2 psmisc \
                    iputils-ping telnet  \
                    imagemagick ruby-full build-essential zlib1g-dev \
-                   php8.1 php8.1-curl php8.1-http php8.1-raphf php8.1-yaml composer linkchecker
+                   php8.3 php8.3-curl php8.3-http php8.3-raphf php8.3-yaml composer linkchecker
 
 
 export GEM_HOME="/usr/bin/gems"
@@ -17,7 +17,9 @@ gem install jekyll bundler jekyll-optional-front-matter jekyll-sitemap
 # Install Node 20 and npm:
 curl -sL https://deb.nodesource.com/setup_20.x | sudo bash -
 sudo apt-get install -y nodejs
-sudo npm install -g npm
+sudo npm install -g npm yarn
+
+sudo composer global require lack/kindergarden
 
 chmod -R 777 /var/www/html
 
