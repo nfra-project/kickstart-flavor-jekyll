5f30237b6434b1b583262c2a2b53fb2406dad01f
Matthias <m@tth.es>
2026-04-30 10:51:52 +0200

    sync

--- kickstart/flavor/build.d/50-install-apache-jekyll.sh
+++ kickstart/flavor/build.d/50-install-apache-jekyll.sh
@@ -15,7 +15,7 @@ export GEM_HOME="/usr/bin/gems"
 gem install jekyll bundler jekyll-optional-front-matter jekyll-sitemap jekyll-polyglot jekyll-paginate jekyll-redirect-from jekyll-lazy-load-image
 
 # Install Node 20 and npm: See https://github.com/nodesource/distributions?tab=readme-ov-file#ubuntu-versions
-curl -sL https://deb.nodesource.com/setup_24.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_25.x | sudo bash -
 sudo apt-get install -y nodejs
 sudo npm install -g npm yarn pnpm
 
